cbe5949ff9bc4a827f3c576c73d01670632d2d3b
Silvia Rebelo <silvi04@gmail.com>
2022-08-21 10:30:56 +0100

    New fragment added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { Fragment } from "react";
-
 import { CreditHeader } from "./components/CreditHeader";
 import { GlobalStyles } from "./components/GlobalStyles";
 import { TicTacToe } from "./components/TicTacToe";
 
 const App = () => (
-  <Fragment>
+  <>
     <GlobalStyles />
     <CreditHeader />
     <TicTacToe />
-  </Fragment>
+  </>
 );
 
 export default App;
